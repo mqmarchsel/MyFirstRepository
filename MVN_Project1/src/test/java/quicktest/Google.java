bb92de19a3c0acef2deaa566ebe822de917eea7e
Vishu <Vishu@Vishu-PC>
2017-04-08 16:17:31 +0530

    New update

--- MVN_Project1/src/test/java/quicktest/Google.java
+++ MVN_Project1/src/test/java/quicktest/Google.java
@@ -1,23 +1,28 @@
 package quicktest;
 
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Google 
 {
-	@Test
-	public void search()
+	static WebDriver driver;
+	public void Open()
 	{
 		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
+	}
+	@Test
+	public void Search()
+	{
+		Open();
 		driver.get("http://www.google.co.in");
 		driver.findElement(By.name("q")).sendKeys("Selenium");
 		driver.findElement(By.name("btnG")).click();
+		driver.close();
 	}
-
 }
